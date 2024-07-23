c472ef74437192df3081fb33d13cfe2c0318a969
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-07-22 21:01:02 -0400

    new structure

--- app/main/main.go
+++ app/main/main.go
@@ -12,8 +12,17 @@ const baseTempFile string = "templates/base.html"
 func home(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "GET" {
-		fmt.Println("GET request received")
+		fmt.Println("GET request received for home")
 		tmpl := template.Must(template.ParseFiles(baseTempFile, "templates/home.html"))
+		tmpl.Execute(w, nil)
+	}
+
+}
+
+func stats(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		fmt.Println("GET request received for stats")
+		tmpl := template.Must(template.ParseFiles(baseTempFile, "templates/stats.html"))
 		tmpl.Execute(w, nil)
 	}
 
@@ -23,6 +32,7 @@ func main() {
 	fmt.Println("Running server on port 8080")
 	fmt.Print("http://127.0.0.1:8080\n\n")
 
+	http.HandleFunc("/stats/", stats)
 	http.HandleFunc("/", home)
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
